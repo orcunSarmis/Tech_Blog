45ed9d4391d007d104a65e593d697058518c6610
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-15 22:18:20 +1000

    Adding dlete handler to dashboard

--- Main/public/js/dashboard.js
+++ Main/public/js/dashboard.js
@@ -19,6 +19,12 @@ const deleteButtonHandler = async (event) => {
             method: 'DELETE',
         });
 
-        
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete article');
+        }
+    } else {
+        alert('Delete button did not have a data-id');
     }
-}
+};
