c135efd58725693713e2b5abe6c1b0b122ebd010
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-15 22:12:58 +1000

    working on dashboard.js

---
+++ Main/public/js/dashboard.js
@@ -0,0 +1,24 @@
+const addButtonHandler - async (event) => {
+    document.location.replace('dashboard/newArticle');
+};
+
+const updateButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+        document.location.replace('/dashboard/updateArticle/${id}');
+    } else {
+        alert('Update button did not have a data-id');
+    }
+};
+
+const deleteButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/article/${id}`, {
+            method: 'DELETE',
+        });
+
+        
+    }
+}
