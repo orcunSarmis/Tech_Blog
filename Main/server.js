4d565253e265f103d2420008bb116fc587d94c14
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-18 20:25:01 +1000

    fixing typo in servre.js

--- Main/server.js
+++ Main/server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.AsyncQueueError({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 });
 
