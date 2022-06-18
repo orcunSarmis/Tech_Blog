401109fb639c4801a2d010d0296b0533e8acee82
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-18 20:37:55 +1000

    working on error in server.js

--- Main/public/js/signin.js
+++ Main/public/js/signin.js
@@ -1,5 +1,16 @@
 const signinFormHandler = async (event) => {
     event.preventDefault();
 
-    
+    const username = document.querySelector('#username-signin').ariaValueMax.trim();
+    const password = document.querySelector('#password-signin').ariaValueMax.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/signin', {
+            method:'POST',
+            body: JSON.stringify({ username, password }),
+            neaders: { 'Content-Type': 'application/jsonm' },
+        });
+
+        
+    }
 }
