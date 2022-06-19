1a26b9891c24632c2dce5c3f4d2a5a0fb303f9a4
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-19 23:17:40 +1000

    adding seed.js

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -5,3 +5,19 @@ const userData = require('./user-seed.json');
 const articleData = require('./article-seed.json');
 const commentData = require('./comment-seed.json');
 
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Article.bulkCreate(articleData);
+
+    await Comment.bulkCreate(commentData);
+
+    process.exit(0);
+
+};
+
+seedDatabase();
