0666a9c1fe617b6f4644ea7133f29dd64e8300bf
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-20 10:50:40 +1000

    Fixing error, database creating, take off if statement from connection, comment out roots from server.js

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -1,22 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-// Password .env details and heroku db setup
-if (process.env.JAWS_URL) {
-    sequelize = new Sequelize(process.env.JAWS_URL);
-}else {
-    sequelize = new Sequelize(
-        process.eventNames.DB_NAME,
-        process.eventNames.DB_USER,
-        process.eventNames.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+const sequelize = process.env.JAWSDB_URL
+ ? new Sequelize(process.env.JAWSDB_URL)
+ : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+     host: 'localhost',
+     dialect: 'mysql',
+     port: 3306
+   });
 
 module.exports = sequelize;
--- Main/server.js
+++ Main/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphs = require('express-handlebars');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 // Lines above, require dependincies for app
 
@@ -24,7 +24,7 @@ const sess = {
         sameSite: 'strict',
     },
     resave: false,
-    saveUsaveUninitialized: true,
+    saveUninitialized: true,
     store: new SequelizeStore({ 
         db: sequelize
     })
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
