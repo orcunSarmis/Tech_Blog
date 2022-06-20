3c2400ed3c0493c9c7dda3b69333f6b55ed57a83
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-20 22:57:33 +1000

    new article.js added

---
+++ Main/public/js/newArticle.js
@@ -0,0 +1,32 @@
+const newArticleFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('new-Article-Title').value.trim();
+    const content = document.getElementById('new-Article-Content').value.trim();
+
+    if (title && contnet) {
+        const response = await fetch('/api/articles/', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alerft(response.statusText);
+        }
+    }
+};
+
+const cancelButtonHandler =async () => {
+    document.location.replace('/dasboard');
+}
+
+document
+.querySelector('.newArticleForm')
+.addEventListener('submit', newArticleFormHandler);
+
+document
+.querySelector('.newArticleForm')
+.addEventListener('reset', cancelButtonHandler);
